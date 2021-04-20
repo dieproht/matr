635c4ffaca3d32ab5ebadb001d071c3411e0a84f
Karl Friedrich Walkow <karl@walkow.de>
2021-04-20 18:27:45 +0200

    Some docs and some cleanups

--- matr-api/shared/src/main/scala/matr/MatrixEquality.scala
+++ matr-api/shared/src/main/scala/matr/MatrixEquality.scala
@@ -1,6 +1,7 @@
 package matr:
 
-
+  /** Type class for testing two Matrices on equality. 
+    */ 
   trait MatrixEquality[R <: Int, C <: Int, T]: 
 
     def matricesEqual(lhs: M, rhs: M): Boolean
--- matr-api/shared/src/main/scala/matr/Submatrix.scala
+++ matr-api/shared/src/main/scala/matr/Submatrix.scala
@@ -1,9 +1,18 @@
 package matr:
 
-  import scala.compiletime.ops.boolean.*
-  import scala.compiletime.ops.int.*
+  import scala.compiletime.ops.boolean.&&
+  import scala.compiletime.ops.int.{<, >=, +, -}
 
-
+  /** Type class for getting the submatrix of a Matrix. 
+    * 
+    * @tparam RowIdxTL Row index of the element that should become the top-left element of the submatrix
+    * @tparam ColIdxTL Column index of the element that should become the top-left element of the submatrix
+    * @tparam RowIdxBR Row index of the element that should become the bottom-right element of the submatrix
+    * @tparam ColIdxBR Column index of the element that should become the bottom-right element of the submatrix
+    * @tparam R Row dimension of the original Matrix
+    * @tparam C Column dimension of the original Matrix
+    * @tparam T Element type of the Matrix
+    */ 
   trait Submatrix[RowIdxTL <: Int, ColIdxTL <: Int, RowIdxBR <: Int, ColIdxBR <: Int, R <: Int, C <: Int, T]
                  (using Submatrix.SliceOK[RowIdxTL, ColIdxTL, RowIdxBR, ColIdxBR, R, C] =:= true)
                  (using Matrix.DimsOK[RowIdxBR - RowIdxTL + 1, ColIdxBR - ColIdxTL + 1] =:= true):
