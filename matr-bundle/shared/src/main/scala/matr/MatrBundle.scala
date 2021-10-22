56bc6a5161c5f6733bed205e6135f3a9e0f99577
Karl F Walkow <38581665+dieproht@users.noreply.github.com>
2021-10-22 22:40:10 +0200

    Tuple-based creation of Matrices (#57)

--- matr-bundle/shared/src/main/scala/matr/MatrBundle.scala
+++ matr-bundle/shared/src/main/scala/matr/MatrBundle.scala
@@ -1,11 +1,11 @@
 package matr:
 
-  import matr.dflt.DefaultMatrixFactory
-  import matr.dflt.DefaultMatrixOps
-  import matr.std.StandardOps
+   import matr.dflt.DefaultMatrixFactory
+   import matr.dflt.DefaultMatrixOps
+   import matr.std.StandardOps
 
-  /** All-in-one import for batteries-included functionality. 
-   * 
-   * Just type ```import matr.MatrBundle.given``` to bring all out-of-the-box givens in scope.  
-   */
-  object MatrBundle extends DefaultMatrixFactory, DefaultMatrixOps, StandardOps
+   /** All-in-one import for batteries-included functionality.
+     *
+     * Just type ```import matr.MatrBundle.given``` to bring all out-of-the-box givens in scope.
+     */
+   object MatrBundle extends DefaultMatrixFactory, DefaultMatrixOps, StandardOps, TupleSupport
