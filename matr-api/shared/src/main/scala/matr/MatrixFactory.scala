b7998e1ffc62e7aefa4ad1b5dca84a241750b3ac
Karl Friedrich Walkow <karl@walkow.de>
2021-10-22 22:33:39 +0200

    Tuple-based creation of Matrices

--- matr-api/shared/src/main/scala/matr/MatrixFactory.scala
+++ matr-api/shared/src/main/scala/matr/MatrixFactory.scala
@@ -1,61 +1,98 @@
-package matr:
+package matr
 
-  import scala.compiletime.ops.any.==
+import scala.compiletime.ops.any.==
+import matr.TupleSupport.MatrixTupleReader
+import matr.TupleSupport.RowTupleReader
 
-  /** Central entry point for creating Matrices. 
-    * Modules implementing the `Matrix` trait should also provide an instance of this type class. 
-    */ 
-  trait MatrixFactory[R <: Int, C <: Int, T]: 
+/** Central entry point for creating Matrices. Modules implementing the `Matrix` trait should also
+  * provide an instance of this type class.
+  */
+trait MatrixFactory[R <: Int, C <: Int, T]:
 
-    /** Returns a new `Builder` instance. 
-      */
-    def builder: MatrixFactory.Builder[R, C, T]
+   /** Returns a new `Builder` instance.
+     */
+   def builder: MatrixFactory.Builder[R, C, T]
 
-    /** Creates a Matrix containing the specified elements, assuming row-major order. 
-      * Dimensions will be checked at runtime. 
-      */ 
-    def rowMajor(elements: T*): Matrix[R, C, T]
+   /** Creates a Matrix containing the specified elements, assuming row-major order. Dimensions will
+     * be checked at runtime.
+     */
+   def rowMajor(elements: T*): Matrix[R, C, T]
 
-    /** Creates a Matrix containing the elements returned by the specified function. 
-      * 
-      * @param fillElem function returning the element at the specified position (row index, column index)
-      */ 
-    def tabulate(fillElem: (Int, Int) => T): Matrix[R, C, T]
-    
-    /** Creates a Matrix of zeros. 
-      */ 
-    def zeros: Matrix[R, C, T]
+   /** Creates a Matrix containing the elements returned by the specified function.
+     *
+     * @param fillElem
+     *   function returning the element at the specified position (row index, column index)
+     */
+   def tabulate(fillElem: (Int, Int) => T): Matrix[R, C, T]
 
-    /** Creates a Matrix of ones. 
-      */ 
-    def ones: Matrix[R, C, T]
+   /** Creates a Matrix of zeros.
+     */
+   def zeros: Matrix[R, C, T]
 
-    /** Creates the identity Matrix. 
-      */ 
-    def identity(using Matrix.IsSquare[R, C] =:= true): Matrix[R, C, T]
+   /** Creates a Matrix of ones.
+     */
+   def ones: Matrix[R, C, T]
 
+   /** Creates the identity Matrix.
+     */
+   def identity(using Matrix.IsSquare[R, C] =:= true): Matrix[R, C, T]
 
-  object MatrixFactory:
+   /** Creates a Matrix with the specified elements that are structured row by row by tuples.
+     *
+     * Dimensions will be checked at compile-time.
+     *
+     * When invoking this method the following import has to be in place:
+     * ```
+     * import matr.TupleSupport.given
+     * ```
+     *
+     * The creation of i.e. a 2x3 Matrix looks like this:
+     * ```
+     * val m = MatrixFactory[2, 3, Int].fromTuple(
+     *   (11, 12, 13),
+     *   (21, 22, 23)
+     * )
+     * ```
+     */
+   def fromTuple[MatrixTuple <: Tuple, RowTuple <: Tuple]
+         (matrixTuple: MatrixTuple)
+         (using valRowDim: ValueOf[R], valColDim: ValueOf[C])
+         (using
+          matrixReader: MatrixTupleReader[MatrixTuple, RowTuple],
+          rowReader: RowTupleReader[RowTuple, T]
+         )
+         (using Tuple.Size[MatrixTuple] =:= R, Tuple.Size[RowTuple] =:= C)
+         : Matrix[R, C, T] =
+      val buildr: MatrixFactory.Builder[R, C, T] = builder
+      val setElem: (Int, Int, T) => Unit =
+         (rowIdx, colIdx, elem) => buildr.update(rowIdx, colIdx, elem)
+      val setRow: (Int, RowTuple) => Unit =
+         (rowIdx, rowTuple) => rowReader.readRow(rowIdx, rowTuple, valColDim.value, setElem)
+      matrixReader.readMatrix(matrixTuple, valRowDim.value, setRow)
+      buildr.result
 
-    /** A `Builder` is a "Matrix under construction" that itself forms a (mutable) Matrix. 
-      * Utilizing `Builder` is the preferred way of creating new Matrices in Matrix operations. 
-      * 
-      * _One_ instance of a `Builder` is used to create _one_ Matrix. This means that invoking the 
-      * same `Builder` instance multiple times is OK, but letting a `Builder` leave a local scope 
-      * is not OK (it's mutable!). 
-      * 
-      * Implementations of this trait must return zero for uninitialized element positions. 
-      */ 
-    trait Builder[R <: Int, C <: Int, T] extends Matrix[R, C, T]: 
- 
-      /** Sets the specified element at the specified position. 
-        */ 
+object MatrixFactory:
+
+   /** A `Builder` is a "Matrix under construction" that itself forms a (mutable) Matrix. Utilizing
+     * `Builder` is the preferred way of creating new Matrices in Matrix operations.
+     *
+     * _One_ instance of a `Builder` is used to create _one_ Matrix. This means that invoking the
+     * same `Builder` instance multiple times is OK, but letting a `Builder` leave a local scope is
+     * not OK (it's mutable!).
+     *
+     * Implementations of this trait must return zero for uninitialized element positions.
+     */
+   trait Builder[R <: Int, C <: Int, T] extends Matrix[R, C, T]:
+
+      /** Sets the specified element at the specified position.
+        */
       def update(rowIdx: Int, colIdx: Int, v: T): this.type
-      
-      /** Creates the Matrix resulting from this `Builder`. 
-        */ 
+
+      /** Creates the Matrix resulting from this `Builder`.
+        */
       def result: Matrix[R, C, T]
 
-    /** Returns the implicitly available `MatrixFactory`. 
-      */ 
-    def apply[R <: Int, C <: Int, T](using mf: MatrixFactory[R, C, T]): MatrixFactory[R, C, T] = mf
+   /** Returns the implicitly available `MatrixFactory`.
+     */
+   def apply[R <: Int, C <: Int, T](using mf: MatrixFactory[R, C, T]): MatrixFactory[R, C, T] =
+      mf
---
+++ matr-tests/shared/src/test/scala/matr/MatrixFactorySpec.scala
@@ -0,0 +1,95 @@
+package test.matr
+
+import matr.ArbitraryMatrix
+import matr.dflt.DefaultMatrixFactory.given
+import matr.dflt.DefaultMatrixOps.given
+import matr.Matrix
+import matr.std.StandardOps.given
+import org.scalacheck.Arbitrary
+import matr.MatrixFactory
+import matr.MatrFlatSpec
+import matr.TupleSupport.given
+
+class MatrixFactorySpec extends MatrFlatSpec:
+
+   "Tuple-built Matrix" should "equal row-major built Matrix" in {
+      val m1: Matrix[4, 3, Int] =
+         MatrixFactory[4, 3, Int].rowMajor(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+      val m2: Matrix[4, 3, Int] =
+         MatrixFactory[4, 3, Int].fromTuple(
+            (1, 2, 3),
+            (4, 5, 6),
+            (7, 8, 9),
+            (10, 11, 12)
+         )
+      m1 === m2 shouldEqual true
+   }
+
+   it should "equal given Matrix 3x2" in {
+
+      given Arbitrary[Matrix[3, 2, Int]] = ArbitraryMatrix[3, 2, Int]
+
+      forAll { (mg: Matrix[3, 2, Int]) =>
+
+         val md: Matrix[3, 2, Int] =
+            MatrixFactory[3, 2, Int].fromTuple(
+               (mg(0, 0), mg(0, 1)),
+               (mg(1, 0), mg(1, 1)),
+               (mg(2, 0), mg(2, 1))
+            )
+
+         md === mg shouldBe true
+      }
+   }
+
+   it should "equal given Matrix 3x4" in {
+
+      given Arbitrary[Matrix[3, 4, Int]] = ArbitraryMatrix[3, 4, Int]
+
+      forAll { (mg: Matrix[3, 4, Int]) =>
+         val md: Matrix[3, 4, Int] =
+            MatrixFactory[3, 4, Int].fromTuple(
+               (mg(0, 0), mg(0, 1), mg(0, 2), mg(0, 3)),
+               (mg(1, 0), mg(1, 1), mg(1, 2), mg(1, 3)),
+               (mg(2, 0), mg(2, 1), mg(2, 2), mg(2, 3))
+            )
+
+         md === mg shouldBe true
+      }
+   }
+
+   it should "not compile with wrong row dimension" in {
+      assertDoesNotCompile("""
+         MatrixFactory[4, 3, Int].fromTuple(
+            (1, 2, 3),
+            (4, 5, 6),
+            (7, 8, 9)
+         )""")
+   }
+
+   it should "not compile with wrong column dimension" in {
+      assertDoesNotCompile("""
+         MatrixFactory[3, 3, Int].fromTuple(
+            (1, 2),
+            (3, 4),
+            (5, 6)
+         )""")
+   }
+
+   it should "not compile with wrong column dimension in one row" in {
+      assertDoesNotCompile("""
+         MatrixFactory[3, 2, Int].fromTuple(
+            (1, 2),
+            (3, 4, 5),
+            (6, 7)
+         )""")
+   }
+
+   it should "not compile with wrong element type" in {
+      assertDoesNotCompile("""
+         MatrixFactory[3, 2, Int].fromTuple(
+            (1, 2),
+            (3, 4.0),
+            (5, 6)
+         )""")
+   }
