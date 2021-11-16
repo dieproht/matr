1769c7f2905c08c9ae52a32474f8cad61d57770c
Karl Friedrich Walkow <karl@walkow.de>
2021-11-16 18:34:31 +0100

    remove old row major index

--- matr-api/shared/src/main/scala/matr/RowMajorIndex.scala
+++
@@ -1,9 +0,0 @@
-package matr.util
-
-/** Utility for converting zero-based element position to one-dimensional row-major index and back.
-  */
-object RowMajorIndex:
-
-   def toIdx(rowIdx: Int, colIdx: Int, numCols: Int): Int = rowIdx * numCols + colIdx
-
-   def fromIdx(idx: Int, numCols: Int): (Int, Int) = (idx / numCols, idx % numCols)
