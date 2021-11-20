efaf9ddf0a1913dd4a496175160089c0c80c76a8
Karl Friedrich Walkow <karl@walkow.de>
2021-11-20 09:57:46 +0100

    Default Matrix Multiplication: Cleanup given's parameters

--- matr-dflt-ops/shared/src/main/scala/matr/dflt/DefaultMatrixMultiplication.scala
+++ matr-dflt-ops/shared/src/main/scala/matr/dflt/DefaultMatrixMultiplication.scala
@@ -1,39 +1,35 @@
 package matr.dflt
 
-  import matr.ElementMultiplication  
-  import matr.Matrix
-  import matr.MatrixFactory
-  import matr.MatrixMultiplication
+import matr.ElementMultiplication
+import matr.Matrix
+import matr.MatrixFactory
+import matr.MatrixMultiplication
 
+trait DefaultMatrixMultiplication:
 
-  trait DefaultMatrixMultiplication:
-    
-    given defaultMatrixMultiplication[R <: Int, C <: Int, L <: Int, T, U, X]
-                                     (using m: ElementMultiplication.Aux[T, U, X])
-                                     (using num: Numeric[X])
-                                     (using mf: MatrixFactory[R, L, X]) 
-                                     (using vr: ValueOf[R], vc: ValueOf[C], vl: ValueOf[L])
-                                     : MatrixMultiplication[R, C, L, T, U] with
-      
-      def dot(lhs: Lhs, rhs: Rhs): Out = 
-        val resultNumCols = vl.value
-        val buildr = mf.builder
-        var j: Int = 0 // lhs col
-        while j < lhs.colDim do 
-          var k: Int = 0 // result row
-          while k < lhs.rowDim do
-            var l: Int = 0 // result col
-            while l < resultNumCols do
-              if j == 0 then
-                buildr(k, l) = m.times(lhs(k, j), rhs(j, l))
-              else
-                buildr(k, l) = num.plus(buildr(k, l), m.times(lhs(k, j), rhs(j, l)))
-              l = l + 1
-            k = k + 1
-          j = j + 1      
-        buildr.result
+   given defaultMatrixMultiplication[R <: Int, C <: Int, L <: Int, T, U, X]
+            (using MatrixFactory[R, L, X], Numeric[X], ElementMultiplication.Aux[T, U, X])
+            (using ValueOf[R], ValueOf[C], ValueOf[L])
+            : MatrixMultiplication[R, C, L, T, U] with
+
+      def dot(lhs: Lhs, rhs: Rhs): Out =
+         val numX = summon[Numeric[X]]
+         val mul = summon[ElementMultiplication.Aux[T, U, X]]
+         val resultNumCols = valueOf[L]
+         val buildr = MatrixFactory[R, L, X].builder
+         var j: Int = 0 // lhs col
+         while j < lhs.colDim do
+            var k: Int = 0 // result row
+            while k < lhs.rowDim do
+               var l: Int = 0 // result col
+               while l < resultNumCols do
+                  if j == 0 then buildr(k, l) = mul.times(lhs(k, j), rhs(j, l))
+                  else buildr(k, l) = numX.plus(buildr(k, l), mul.times(lhs(k, j), rhs(j, l)))
+                  l = l + 1
+               k = k + 1
+            j = j + 1
+         buildr.result
 
       type OutT = X
 
-      
-  object DefaultMatrixMultiplication extends DefaultMatrixMultiplication
+object DefaultMatrixMultiplication extends DefaultMatrixMultiplication
