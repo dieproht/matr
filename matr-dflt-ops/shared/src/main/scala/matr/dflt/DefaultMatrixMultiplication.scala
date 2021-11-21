c3c90e3d0a4ef70db6c1bd422f577a493e7ee31b
Karl Friedrich Walkow <karl@walkow.de>
2021-11-21 11:07:05 +0100

    cleanup matrix multiplication parameters

--- matr-dflt-ops/shared/src/main/scala/matr/dflt/DefaultMatrixMultiplication.scala
+++ matr-dflt-ops/shared/src/main/scala/matr/dflt/DefaultMatrixMultiplication.scala
@@ -8,17 +8,15 @@ import matr.MatrixMultiplication
 trait DefaultMatrixMultiplication:
 
    given defaultMatrixMultiplication[R <: Int, C <: Int, L <: Int, T, U, X]
-            (using
-             em: ElementMultiplication.Aux[T, U, X],
-             num: Numeric[X],
-             mf: MatrixFactory[R, L, X]
-            )
-            (using vr: ValueOf[R], vc: ValueOf[C], vl: ValueOf[L])
+            (using ElementMultiplication.Aux[T, U, X], Numeric[X], MatrixFactory[R, L, X])
+            (using ValueOf[R], ValueOf[C], ValueOf[L])
             : MatrixMultiplication[R, C, L, T, U, X] with
 
       def dot(lhs: Lhs, rhs: Rhs): Out =
-         val resultNumCols = vl.value
-         val buildr = mf.builder
+         val elemMul = summon[ElementMultiplication.Aux[T, U, X]]
+         val numX = summon[Numeric[X]]
+         val resultNumCols = valueOf[L]
+         val buildr = MatrixFactory[R, L, X].builder
          var j: Int = 0 // lhs col
          while j < lhs.colDim do
             var k: Int = 0 // result row
@@ -26,9 +24,9 @@ trait DefaultMatrixMultiplication:
                var l: Int = 0 // result col
                while l < resultNumCols do
                   if j == 0 then
-                     buildr(k, l) = em.times(lhs(k, j), rhs(j, l))
+                     buildr(k, l) = elemMul.times(lhs(k, j), rhs(j, l))
                   else
-                     buildr(k, l) = num.plus(buildr(k, l), em.times(lhs(k, j), rhs(j, l)))
+                     buildr(k, l) = numX.plus(buildr(k, l), elemMul.times(lhs(k, j), rhs(j, l)))
                   l = l + 1
                k = k + 1
             j = j + 1
