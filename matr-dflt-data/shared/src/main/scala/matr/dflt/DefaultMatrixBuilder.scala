3f2d53b03576067d53749581494fbb6b3a16fb51
Karl F Walkow <karl@walkow.de>
2025-02-21 08:08:52 +0100

    [WIP] DefaultMatrixBuilder instead of DefaultMatrixFactory

---
+++ matr-dflt-data/shared/src/main/scala/matr/dflt/DefaultMatrixBuilder.scala
@@ -0,0 +1,58 @@
+package matr.dflt
+
+import matr.Matrix
+import matr.util.RowMajorIndex
+
+import scala.collection.mutable
+import scala.reflect.ClassTag
+
+case class DefaultMatrixBuilder[R <: Int, C <: Int, T]
+         ()
+         (using Numeric[T])
+         (using Matrix.Requirements.NonNegativeDimensions[R, C])
+         (using ValueOf[R], ValueOf[C])
+    extends Matrix.Builder[R, C, T]:
+
+   private val num: Numeric[T] = summon[Numeric[T]]
+
+   private var elemMap: mutable.Map[(Int, Int), T] = mutable.Map.empty
+
+   private var elemArr: Array[T] = null
+
+   private val treshold: Int = (rowDim * colDim * DefaultMatrixBuilder.MIN_DENSE_FILL).floor.toInt
+
+   override def apply(rowIdx: Int, colIdx: Int): T =
+      if elemMap ne null then
+         elemMap.getOrElse((rowIdx, colIdx), num.zero)
+      else
+         elemArr(RowMajorIndex.toIdx(rowIdx, colIdx, valueOf[C]))
+
+   override def update(rowIdx: Int, colIdx: Int, v: T): this.type =
+      if (elemMap ne null) && (elemMap.size < treshold) then
+         if v != num.zero then
+            elemMap((rowIdx, colIdx)) = v
+      else
+         if elemArr eq null then
+            given ClassTag[T] = ClassTag(v.getClass)
+            elemArr = Array.fill(rowDim * colDim)(num.zero)
+            elemMap
+               .foreachEntry((mk, mv) => elemArr(RowMajorIndex.toIdx(mk._1, mk._2, colDim)) = mv)
+            elemMap = null
+         elemArr(RowMajorIndex.toIdx(rowIdx, colIdx, valueOf[C])) = v
+      this
+
+   override def result: Matrix[R, C, T] =
+      if elemArr eq null then
+         new DefaultSparseMatrix[R, C, T](elemMap.toMap)
+      else
+         new DefaultDenseMatrix[R, C, T](elemArr)
+
+object DefaultMatrixBuilder:
+   val MIN_DENSE_FILL: Float = 0.5
+
+   given newDefaultMatrixBuilder[R <: Int, C <: Int, T]
+            (using Numeric[T])
+            (using Matrix.Requirements.NonNegativeDimensions[R, C])
+            (using ValueOf[R], ValueOf[C])
+            : Matrix.Builder[R, C, T] = DefaultMatrixBuilder()
+   // TODO verify that method is invoked on every Matrix construction
